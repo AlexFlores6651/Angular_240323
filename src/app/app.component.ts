0c215c627d8a0aa8d9b4007383dc44bac59ec525
AlexFlores6651 <106271632+AlexFlores6651@users.noreply.github.com>
2023-03-24 09:34:50 -0600

    Archivo Completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  datos: personas[] = [
+    { id: 1, nombre: 'Alejandro', apellido: 'Flores', sexo: 'Masculino' },
+    { id: 2, nombre: 'Fernanda', apellido: 'Diaz', sexo: 'Femenino' },
+    { id: 3, nombre: 'Iris', apellido: 'Gloria', sexo: 'Femenino' },
+    { id: 4, nombre: 'Manuel', apellido: 'Ramirez', sexo: 'Masculino' },
+    { id: 5, nombre: 'Luis', apellido: 'Sanchez', sexo: 'Masculino' }
+  ];
+} //fin clase
+
+interface personas {
+  id: number;
+  nombre: string;
+  apellido: string;
+  sexo: string;
 }
